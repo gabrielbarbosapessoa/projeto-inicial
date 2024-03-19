a20f5d414efd6e33201ee660f3c02e9a0d3f1e0f
gabrielbarbosapessoa2@gmail.com <gabrielbarbosapessoa2@gmail.com>
2024-03-19 08:23:12 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
 var nome = document.getElementById("nome");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var CEP = document.getElementById("CEP");
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("complemento");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
+
+var saida = document.getElementById("saida-de-dados");
 
 function alertar(event){
-    alert("Você clicou no botão!!!! " + nome.value);
+    //alert("Você clicou no botão!!!! " + nome.value);
+
+    saida.innerText = "Nome:  " +nome.value + 
+             "\n Email:  " +email.value +
+             "\n Telefone:  " + telefone.value +
+             "\n CEP:  " + CEP.value +
+             "\n Logradouro:  " + logradouro.value +
+             "\n Numero:  " + numero.value +
+             "\n Complemento:  " + complemento.value +
+             "\n Bairro:  " + bairro.value +
+             "\n Cidade:  " + cidade.value + 
+             "\n Estado:  " + estado.value; 
 }
